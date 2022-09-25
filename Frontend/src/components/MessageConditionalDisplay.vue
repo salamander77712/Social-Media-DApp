f77b6cf83480119d912e8e170a5099bcb1adcf91
PickleSalamander <56451516+salamander77712@users.noreply.github.com>
2022-09-25 01:20:51 -0400

    Create MessageConditionalDisplay.vue

---
+++ Frontend/src/components/MessageConditionalDisplay.vue
@@ -0,0 +1,30 @@
+<script>
+import MassDisplayMessage from './MassDisplayMessage.vue'
+  export default{
+    /*
+    {
+      (all optional)
+      comments: true,
+      author: {
+        is: 'address',
+        isNot: 'address'
+      },
+      id: {
+        is: 'id',
+        isNot: 'id',
+        greaterThan: idNumber,
+        lessThan: idNumber
+      }
+    }
+    */
+    props: ['searchParams'],
+    components: {
+      MassDisplayMessage
+     },
+     
+  }
+</script>
+
+<template>
+  <MassDisplayMessage :datas=messages></MassDisplayMessage>
+</template>
