a216bfdfd97b6e0e232d867a44806fc1371de24d
PickleSalamander <56451516+salamander77712@users.noreply.github.com>
2022-09-25 00:21:25 -0400

    Create MassDisplayMessage.vue

---
+++ Frontend/src/components/MassDisplayMessage.vue
@@ -0,0 +1,15 @@
+<script>
+import MessageDisplay from './MessageDisplay.vue'
+  export default{
+    props: ['datas'],
+    components: {
+      MessageDisplay
+    }
+  }
+</script>
+
+<template>
+  <div v-for="data in datas">
+    <MessageDisplay :event=data></MessageDisplay>
+  </div>
+</template>
