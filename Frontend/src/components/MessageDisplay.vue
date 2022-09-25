86b0686585f8b0070c0472b3240ad0bce18d20fb
PickleSalamander <56451516+salamander77712@users.noreply.github.com>
2022-09-25 00:43:21 -0400

    Update MessageDisplay.vue

--- Frontend/src/components/MessageDisplay.vue
+++ Frontend/src/components/MessageDisplay.vue
@@ -1,43 +1,26 @@
 <script>
   export default {
+    /*
+    {
+      contentHistory: ["mostRecentEdit", ..., "OrigonalContent"],
+      author: "address",
+      id: idNumber,
+      username: "username",
+      tips: numberOfTips,
+      (optional) deleted: true,
+      (optional) comments: [{commentsInSameFormat}, ..., {comments to comments are nested}],
+      (optional) numberOfComments: numberOfComments
+    }
+    
+    */
     props: ['event'],
     data() {
       return {
-        username: '',
-        tips: 0,
         tipAmount: '',
-        deleted: false,
-        account: undefined,
         editText: ''
       }
     },
     methods: {
-      async getUsername(){
-        let temp;
-        await contract.getPastEvents("NewUsername", {fromBlock: 0, toBlock:"latest"}).then((events) =>{
-          temp = events;
-          for(let i = temp.length - 1; i >= 0; i--){
-          if(temp[i].returnValues.user.toUpperCase() == this.event.returnValues.author.toUpperCase()){
-            this.username = temp[i].returnValues.name;
-            return;
-          }
-        }
-        this.username = this.address.address;
-        });
-      },
-      async getTips(){
-        let temp;
-        await contract.getPastEvents("MessageTipped", {fromBlock: 0, toBlock:"latest"}).then((events) =>{
-          temp = events;
-          for(let i = 0; i < temp.length; i++){
-          if(temp[i].returnValues.author.toUpperCase() == this.event.returnValues.author.toUpperCase()){
-            if(temp[i].returnValues.messageID == this.event.returnValues.id){
-              this.tips += Number(temp[i].returnValues.tipAmount);
-            }
-          }
-        }
-        });
-      },
       async tip(){
         contract.methods.tipMessage(this.event.returnValues.author, this.event.returnValues.id).send({ from: account, value: this.tipAmount});
       },
@@ -46,32 +29,6 @@
       },
       async deleteMessage(){
         contract.methods.deleteMessage(this.event.returnValues.id).send({ from: account});
-      },
-      async checkDeleted(){
-        let temp;
-        await contract.getPastEvents("MessageDeleted", {fromBlock: 0, toBlock:"latest"}).then((events) =>{
-          temp = events;
-          for(let i = 0; i < temp.length; i++){
-          if(temp[i].returnValues.author.toUpperCase() == this.event.returnValues.author.toUpperCase()){
-            if(temp[i].returnValues.id == this.event.returnValues.id){
-              this.deleted = !this.deleted;
-            }
-          }
-        }
-        });
-      },
-      async getLatestEdit(){
-        let temp;
-        await contract.getPastEvents("MessageEdited", {fromBlock: 0, toBlock:"latest"}).then((events) =>{
-          temp = events;
-          for(let i = temp.length - 1; i >= 0; i--){
-          if(temp[i].returnValues.author.toUpperCase() == this.event.returnValues.author.toUpperCase()){
-            if(temp[i].returnValues.id == this.event.returnValues.id){
-              this.event.returnValues.content = temp[i].returnValues.newContent;
-            }
-          }
-        }
-        });
       },
       async editMessage(){
         contract.methods.editMessage(this.event.returnValues.id, this.editText).send({ from: account});
@@ -85,7 +42,7 @@
         return (account.toUpperCase() == this.event.returnValues.author.toUpperCase());
       },
       deletedCSS(){
-        if(this.deleted){
+        if(this.event.deleted){
           return 'deleted';
         }
         else{
@@ -100,32 +57,31 @@
           return 'Delete';
         }
       }
-    },
-      created(){
-      this.getUsername();
-      this.getTips();
-      this.checkDeleted();
-      this.getLatestEdit();
     }
     }
   </script>
   
   <template>
-    <div class="container" v-if="!deleted || isUser">
-      <h2 class="username" :class="deletedCSS">{{username}}</h2>
-      <a @click="goToAuthor" :class="deletedCSS">@{{event.returnValues.author}}</a>
-      <p class="messageID" :class="deletedCSS">{{event.returnValues.id}}</p>
-      <p class="tips" :class="deletedCSS">Tips: {{tips}}</p>
-      <p class="content" :class="deletedCSS">{{event.returnValues.content}}</p>
+    <div class="container" v-if="!event.deleted || isUser">
+      <h2 class="username" :class="deletedCSS">{{event.username}}</h2>
+      <a @click="goToAuthor" :class="deletedCSS">@{{event.author}}</a>
+      <p class="messageID" :class="deletedCSS">{{eventid}}</p>
+      <p class="tips" :class="deletedCSS">Tips: {{event.tips}}</p>
+      <p class="content" :class="deletedCSS">{{event.contentHistory[0]}}</p>
       <div class="buttons">
         <input type="number" v-model="tipAmount"/>
         <button @click="tip">Tip</button>
         <br>
-        <button @click="$router.push('/u/' + event.returnValues.author + '/p/' + event.returnValues.id)">View Comments</button>
+        <button @click="$router.push('/u/' + event.returnValues.author + '/p/' + event.returnValues.id)" v-if="event.comments == undefined">View {{event.numberOfComments}} Comments</button>
         <button v-if="isUser" @click="deleteMessage">{{deleteButtonText}}</button>
         <br v-if="isUser">
         <input type="text" v-model="editText" v-if="isUser">
         <button @click="editMessage" v-if="isUser">Edit</button>
+      </div>
+      <div class="commentContainer" v-if="event.comments">
+        <div class="comment" v-for="comment in event.comments">
+          <MessageDisplay :event="comment"></MessageDisplay>
+        </div>
       </div>
     </div>
   </template>
@@ -149,5 +105,13 @@
     }
     .deleted{
       text-decoration: line-through;
+    }
+    .commentContainer{
+      width: 200px;
+      margin: 0 auto;
+    }
+
+    .comment{
+      margin-left: 50px;
     }
   </style>
