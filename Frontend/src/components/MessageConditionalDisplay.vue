c2401e08a76e91ad5563f53e62b20fede30783df
PickleSalamander <56451516+salamander77712@users.noreply.github.com>
2022-09-25 02:09:34 -0400

    Update MessageConditionalDisplay.vue

--- Frontend/src/components/MessageConditionalDisplay.vue
+++ Frontend/src/components/MessageConditionalDisplay.vue
@@ -6,14 +6,28 @@ import MassDisplayMessage from './MassDisplayMessage.vue'
       (all optional)
       comments: true,
       author: {
-        is: 'address',
-        isNot: 'address'
+        is: ['address'],
+        isNot: ['address']
       },
       id: {
-        is: 'id',
-        isNot: 'id',
+        is: ['id]',
+        isNot: ['id'],
         greaterThan: idNumber,
         lessThan: idNumber
+      },
+      username: {
+        is: ['username'],
+        isNot: ['username'],
+        contains: ['user'],
+        doesNotContain: ['name']
+      },
+      content: {
+        contains: ['content'],
+        doesNotContain: ['content']
+      },
+      tips: {
+        greaterThan: number,
+        lessThan: number
       }
     }
     */
@@ -21,7 +35,51 @@ import MassDisplayMessage from './MassDisplayMessage.vue'
     components: {
       MassDisplayMessage
      },
-     
+     data (){
+      return {
+      messages: []
+      }
+     },
+     methods: {
+      async getMessages(){
+        await contract.getPastEvents("NewMessage", {fromBlock: 0, toBlock:"latest"}).then((events) => {
+          let foramttedMessages = [];
+          for(let i = 0; i < events.length; i++){
+            let message = events[i].returnValues;
+            if(this.searchParams.author.is){
+              if(!this.searchParams.author.is.includes(message.author)){
+                continue;
+              }
+            }
+            if(this.searchParams.author.isNot){
+              if(this.searchParams.author.isNot.inclues(message.author){
+                continue;
+               }
+            }
+            if(this.searchParams.id.is){
+              if(!this.searchParams.id.is.includes(message.id)){
+                continue;
+               }
+            }
+            if(this.searchParams.id.isNot){
+              if(this.searchPArams.id.isNot.indcludes(message.id)){
+                continue;
+              }
+            }
+            if(this.searchParams.id.greaterThan){
+              if(message.id <= this.searchParams.id.greaterThan){
+                continue;
+              }
+            }
+            if(this.searchParams.id.lessThan){
+              if(message.id >= this.searchParams.id.lessThan){
+                continue;
+              }
+            }
+          }
+        });
+      }
+     }
   }
 </script>
 
